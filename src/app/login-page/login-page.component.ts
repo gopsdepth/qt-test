7407d4ae6ba52b26658eaa7bee0a58616d2cc30d
Satjapot Iamopas <gopsdepth@gmail.com>
2023-02-18 01:18:23 +0700

    Implement Login Page UI

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  isError = false;
 
+  constructor(private readonly router: Router) {}
+
+  login(username: string, password: string) {
+    if (username == 'admin' && password == 'test123') {
+      this.router.navigate(['fund-list']);
+    } else {
+      this.isError = true;
+    }
+  }
 }
